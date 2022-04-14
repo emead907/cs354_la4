0097da06020818eef8c049ddf77c848b0aea7fec
Emily Mead <42479682+emead907@users.noreply.github.com>
2022-04-14 15:29:16 -0600

    Update checking.go

--- checking.go
+++ checking.go
@@ -14,7 +14,7 @@ func (c *Checking) Accure(rate float32, accrue chan float32) {
 	c.Account.Accrue(rate, accrue)
 }
 
-func newChecking(number string, customer Customer, balance float32) (c *Checking) {
+func NewChecking(number string, customer Customer, balance float32) (c *Checking) {
 	c = new(Checking)
 	c.Init(number, customer, balance)
 	return
